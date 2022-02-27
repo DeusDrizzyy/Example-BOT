3b52288689c66143497645f8f0810bb8ade9740b
DeusDrizzyy <66096216+DeusDrizzyy@users.noreply.github.com>
2022-02-27 14:38:38 -0300

    Update getFiles.js

--- functions/getFiles.js
+++ functions/getFiles.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 
 function searchFiles(s, e = "js") {
-	let n = fs.readdirSync(s);
-	const t = [], o = [];
-	for (const r of n)
-		if (1 !== r.split(".").length) r.split(".").pop() === e && o.push(`${s}/${r}`);
-		else {
-			const n = searchFiles(`${s}/${r}`, e);
-			t.push(...n);
-		}
-	return o.length > 0 && t.push({
-		directory: s,
-		files: o
-	}) && t;
+    let n = fs.readdirSync(s);
+    const t = [], o = [];
+    for (const r of n)
+        if (1 !== r.split(".").length) r.split(".").pop() === e && o.push(`${s}/${r}`);
+        else {
+            const n = searchFiles(`${s}/${r}`, e);
+            t.push(...n);
+        }
+    return o.length > 0 && t.push({
+        directory: s,
+        files: o
+    }) && t;
 }
 
 module.exports.searchFiles = searchFiles;
